2c4798dc5e711d8e5a0a98fa41a956d5d7035471
Hunter Thompson <thompson.grey.hunter@gmail.com>
2021-03-08 22:49:05 -0800

    Added test file skeleton

---
+++ tests/test_fasta_frequency.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestFastaFrequency(unittest.TestCase):
+    pass
---
+++ tests/test_fastq_percentage.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestFastqPercentage(unittest.TestCase):
+
+    def test_directory_empty(self):
+        pass
+
+    def test_directory_dne_failure(self):
+        pass
+
+    def test_file
