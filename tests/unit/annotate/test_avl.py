cddd1808e973e6146fd25e47df8443de2aecbc16
Hunter Thompson <thompson.grey.hunter@gmail.com>
2021-03-16 02:13:03 -0700

    Added test harness for AVL tree

---
+++ tests/unit/annotate/test_avl.py
@@ -0,0 +1,53 @@
+import unittest
+from unittest import mock
+from unittest.mock import patch
+
+from bio.annotate import avl
+
+
+class TestRadixSort(unittest.TestCase):
+
+    def test_calc_balance(self):
+        pass
+
+    def test_calc_height(self):
+        pass
+
+    def test_merge(self):
+        pass
+
+    def test_right_rotate(self):
+        pass
+
+    def test_left_rotate(self):
+        pass
+
+    def test_insert_empty_root(self):
+        pass
+
+    def test_insert_left_insert_called(self):
+        pass
+
+    def test_insert_right_insert_called(self):
+        pass
+
+    def test_insert_merge_called(self):
+        pass
+
+    def test_insert_rr_called(self):
+        pass
+
+    def test_insert_ll_called(self):
+        pass
+
+    def test_insert_lr_called(self):
+        pass
+
+    def test_search_node_unkown(self):
+        pass
+
+    def test_search_coordinate_less_than_root(self):
+        pass
+
+    def test_search_coordinate_greater_than_root(self):
+        pass
