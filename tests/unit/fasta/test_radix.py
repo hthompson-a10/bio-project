ecb96fd1cfa520c5c98b95d9bd6ab02f0c45df11
Hunter Thompson <thompson.grey.hunter@gmail.com>
2021-03-15 22:57:38 -0700

    Added tests for radix sort method

--- tests/unit/fasta/test_radix.py
+++ tests/unit/fasta/test_radix.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest import mock
+from unittest.mock import patch
 
 from bio.fasta import radix
 
 
-class TestFrequency(unittest.TestCase):
+class TestRadixSort(unittest.TestCase):
 
     def test_countingsort_empty_no_failure(self):
         actual_list = []
@@ -37,3 +38,23 @@ class TestFrequency(unittest.TestCase):
         expected_list = [mock_1, mock_2, mock_3]
         radix.RadixSort()._counting_sort(actual_list, 1)
         self.assertEqual(actual_list, expected_list)
+
+    def test_sort_max_is_ones(self):
+        with patch.object(radix.RadixSort, '_counting_sort') as mock_sort:
+            radix.RadixSort().sort([], 1)
+        self.assertEqual(mock_sort.call_count, 1)
+
+    def test_sort_max_is_tens(self):
+        with patch.object(radix.RadixSort, '_counting_sort') as mock_sort:
+            radix.RadixSort().sort([], 10)
+        self.assertEqual(mock_sort.call_count, 2)
+
+    def test_sort_max_is_hundreds(self):
+        with patch.object(radix.RadixSort, '_counting_sort') as mock_sort:
+            radix.RadixSort().sort([], 100)
+        self.assertEqual(mock_sort.call_count, 3)
+
+    def test_sort_max_is_thousands(self):
+        with patch.object(radix.RadixSort, '_counting_sort') as mock_sort:
+            radix.RadixSort().sort([], 1000)
+        self.assertEqual(mock_sort.call_count, 4)
