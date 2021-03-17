03929bdfb8b33409554cf49b43a7f83565bc12ab
Hunter Thompson <thompson.grey.hunter@gmail.com>
2021-03-16 20:55:29 -0700

    Added integration tests for annotate

--- tests/unit/annotate/test_annotate.py
+++ tests/unit/annotate/test_annotate.py
@@ -1,4 +1,5 @@
 import copy
+import os
 import unittest
 from unittest import mock
 from unittest.mock import mock_open
@@ -35,7 +36,7 @@ class TestAnnotate(unittest.TestCase):
     def test_buildtreemap_duplicate_chromosome(self, mock_annotation, mock_tree, mock_root):
         anno_1 = mock.Mock(gene_name='mock_gene', chromosome='chr1')
         anno_2 = mock.Mock(gene_name='mock_gene', chromosome='chr1')
-        annotate._create_annotation_node = lambda x : anno_1 if x == 'line1\n' else anno_2
+        mock_annotation.side_effect = [anno_1, anno_2]
         mock_lines = self._format_filedata(['line1', 'line2'])
         with mock.patch("builtins.open", mock_open(read_data=mock_lines)) as mock_file:
             result = annotate.build_tree_map(['mock_file_path'])
@@ -43,11 +44,45 @@ class TestAnnotate(unittest.TestCase):
 
 
     @mock.patch.object(avl.AVLGeneTree, 'insert')
-    def test_buildtreemap_no_duplicate_chromosomes(self, mock_tree):
+    @mock.patch('bio.gtf.annotate._create_annotation_node')
+    def test_buildtreemap_no_duplicate_chromosomes(self, mock_annotation, mock_tree):
         anno_1 = mock.Mock(gene_name='mock_gene', chromosome='chr1')
         anno_2 = mock.Mock(gene_name='mock_gene2', chromosome='chr2')
-        annotate._create_annotation_node = lambda x : anno_1 if x == 'line1\n' else anno_2
+        mock_annotation.side_effect = [anno_1, anno_2]
         mock_lines = self._format_filedata(['line1', 'line2'])
         with mock.patch("builtins.open", mock_open(read_data=mock_lines)) as mock_file:
             result = annotate.build_tree_map(['mock_file_path'])
         self.assertEqual(len(result.keys()), 2)
+
+
+class TestAnnotateIntegration(unittest.TestCase):
+
+    def test_buildtreemap_map_has_two_entries(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        fixture_path = "../../fixtures/gtf/hg19_fixture1.gtf"
+        fixture_path = os.path.join(dir_path, fixture_path)
+        tree_map = annotate.build_tree_map(fixture_path)
+        self.assertEqual(tree_map['chr3'].start, 134196546)
+        self.assertEqual(tree_map['chr3'].end, 134204866)
+        self.assertEqual(tree_map['chr9'].start, 136325087)
+        self.assertEqual(tree_map['chr9'].end, 136335910)
+
+    def test_buildtreemap_root_has_left_node(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        fixture_path = "../../fixtures/gtf/hg19_fixture2.gtf"
+        fixture_path = os.path.join(dir_path, fixture_path)
+        tree_map = annotate.build_tree_map(fixture_path)
+        self.assertEqual(tree_map['chr3'].start, 134196546)
+        self.assertEqual(tree_map['chr3'].end, 134204866)
+        self.assertEqual(tree_map['chr3'].left_node.start, 38589553)
+        self.assertEqual(tree_map['chr3'].left_node.end, 38691164)
+
+    def test_buildtreemap_root_has_right_node(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        fixture_path = "../../fixtures/gtf/hg19_fixture3.gtf"
+        fixture_path = os.path.join(dir_path, fixture_path)
+        tree_map = annotate.build_tree_map(fixture_path)
+        self.assertEqual(tree_map['chr3'].start, 38589553)
+        self.assertEqual(tree_map['chr3'].end, 38691164)
+        self.assertEqual(tree_map['chr3'].right_node.start, 134196546)
+        self.assertEqual(tree_map['chr3'].right_node.end, 134204866)
